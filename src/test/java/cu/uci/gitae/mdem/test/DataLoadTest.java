0580f7bb4cb85ee0442df910de62e32e348d7e48
angel <angel@angel-laptopw>
2018-12-09 09:59:38 +0100

    cambiando las pruebas

--- src/test/java/cu/uci/gitae/mdem/test/DataLoadTest.java
+++ src/test/java/cu/uci/gitae/mdem/test/DataLoadTest.java
@@ -14,6 +14,7 @@ import java.util.stream.Stream;
 import org.apache.spark.SparkConf;
 import org.apache.spark.SparkContext;
 import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Encoders;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 import org.junit.After;
@@ -56,16 +57,39 @@ public class DataLoadTest {
     // TODO add test methods here.
     // The methods must be annotated with annotation @Test. For example:
     //
-//    @Test
+    @Test
     public void dataLoad() {
-        String pathToDataset = "./data/dataset2.tsv";
+        String pathToDataset = "./data/Assistments_Math_2006_2007.txt";
         DataLoad dataLoad = new DataLoadImpl();
         Map<String, String> param = new HashMap<>();
         param.put("datasetPath", pathToDataset);
         Dataset<Row> dataset = dataLoad.loadData(sparkSession, DataSourceType.TSV, param);
         assertNotNull(dataset);
         System.out.println(dataset.count());
-    }
+        dataset.printSchema();
+        Dataset<Row> estudiantes = dataset
+                //.select("Anon Student Id")
+                .select("KC (MCAS39-State_WPI-Simple)")
+                .distinct();
+        System.out.println("KC: " + estudiantes.count());
+//        final Dataset<Row> est_hab = dataset.select("Anon Student Id", "KC(SubSkills)");
 
+//        Long sum = 0L;
+//        Long cont = 0L;
+//
+//        List<Row> list = estudiantes.collectAsList();
+//        for (Row row : list) {
+//            Long count;
+//            count = est_hab
+//                    .filter(est_hab.col("Anon Student Id").equalTo(row.getString(0)))
+//                    .select("KC(SubSkills)").distinct()
+//                    .count();
+//            System.out.println("[Estudiante: " + row.getString(0) + " - Habilidades: " + count + "]");
+//            sum += count;
+//            cont++;
+//        }
+//        System.out.println("Sum: "+sum+" - Cont: "+cont);
+//        System.out.println("Promedio: "+(double)sum/cont);
+    }
 
 }
